be9ca246275be3d19a157e5b038bbe7e9c7720d7
José Lucas Alves da Silva <lukasalvespod1@gmail.com>
2020-08-14 04:06:38 -0300

    using the dynamodb, defining iam role statements and optimissing serverless.yml

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,12 +1,23 @@
+import {v4 as uuid} from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: 'OPEN',
-    createdEt: now.toISOString(),
+    createdAt: now.toISOString(),
   };
+
+  await dynamoDb.put({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction,
+  }).promise();
 
   return {
     statusCode: 201,
