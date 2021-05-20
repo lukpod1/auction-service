c2f3959f66f493c1a118da6a0c89d84959b71201
José Lucas Alves da Silva <lukasalvespod1@gmail.com>
2021-05-20 00:20:31 -0300

    handling no bids case

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -23,12 +23,24 @@ export async function closeAuction(auction) {
     const { title, seller, highestBid } = auction;
     const { amount, bidder } = highestBid;
 
+    if (amount === 0 ) {
+        await sqs.sendMessage({
+            QueueUrl: process.env.MAIL_QUEUE_URL,
+            MessageBody: JSON.stringify({
+                subject: 'No bids on your auction item :(',
+                recipient: seller,
+                body: `Oh no! Your item "${title}" didn"t get any bids. Better luck next time!`
+            })
+        }).promise();
+        return;
+    }
+
     const notifySeller = sqs.sendMessage({
         QueueUrl: process.env.MAIL_QUEUE_URL,
         MessageBody: JSON.stringify({
-            subject: 'Your tem has been sold!',
+            subject: 'Your item has been sold!',
             recipient: seller,
-            body: `Woohoo! Your tem "${title}" has been sold for ${amount}`
+            body: `Woohoo! Your item "${title}" has been sold for $${amount}.`
         })
     }).promise();
 
@@ -37,7 +49,7 @@ export async function closeAuction(auction) {
         MessageBody: JSON.stringify({
             subject: 'You won an auction!',
             recipient: bidder,
-            body: `What a great deal!You got yourself a "${title}" for ${amount}`
+            body: `What a great deal! You got yourself a "${title}" for $${amount}.`
         })
     }).promise();
 
